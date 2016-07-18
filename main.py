2c3c74c5bf531a23128a40bbeb977247b47b2a30
normeow <rgdar@yandex.ru>
2016-07-19 01:19:36 +0300

    fix some syntax issues

--- main.py
+++ main.py
@@ -12,28 +12,42 @@ class NeuralNetwork:
         class Neuron:
             def __init__(self, syn_count, bias):
                 self.bias = bias
-                self.weights = [np.random.randn(i, 1) for i in range(syn_count)]
+                self.weights = np.random.randn(syn_count, 1)
 
             def activate(self, x):
-                return sigmoid(np.dot(x, self.weights) + self.bias)
+                s = sigmoid(np.dot(x, self.weights) + self.bias)
+                return s
 
-        def __init__(self, count, syn_per_neuron):
-            biases = [np.random.randn(i, 1) for i in range(count)]
-            self.neurons = [self.Neuron(syn_per_neuron, biases[i]) for i in range(count)]
+
+        def __init__(self, bias, count, syn_per_neuron):
+            self.neurons = [self.Neuron(syn_per_neuron, bias) for i in range(count)]
 
         def feed_forward(self, inputs):
-            return [self.neurons[i].activate(inputs[i]) for i in len(self.neurons)]
+            self.inputs = inputs
+            self.outputs = [n.activate(self.inputs) for n in self.neurons]
 
-    def __init__(self, sizes):
-        self.layers = [self.NeuralLayer(sizes[i],3) for i in range(len(sizes))]
+    def __init__(self, sizes, learning_rate = 0.5):
+        pass
+        #self.layers = [self.NeuralLayer(bias, sizes[i],) for i in range(len(sizes))]
+
 
     def feed_forward(self, x):
-        """
+        o = self.layers[0].feed_forward(x)
+        for layer in self.layers[1:]:
+            o = layer.feed_forward(o)
+        return o
 
-        :param x: inputs
-        :return: output on inputs
-        """
+    def train(self, inputs, answers):
+        for i in range(len(inputs)):
+            output = self.feed_forward(inputs[i])
+            q_errors = np.dot(0.5*(answers[i] - output))
 
-        pass
 
 jimmy = NeuralNetwork([2,2,1])
+X = np.array([[0, 1],
+              [1, 0],
+              [0, 0],
+              [1, 1]])
+# output matrix
+Y = np.array([[1, 1, 0, 0]])
+#jimmy.train(X,Y.T)
