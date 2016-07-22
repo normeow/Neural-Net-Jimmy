b2aeb545c30e59388ec3a5e392c2a7450ab5dc04
normeow <rgdar@yandex.ru>
2016-07-22 23:43:07 +0300

    add some methods, I tried to implement backpropagation. I tried.

--- main.py
+++ main.py
@@ -10,26 +10,54 @@ class NeuralNetwork:
     class NeuralLayer:
 
         class Neuron:
-            def __init__(self, syn_count, bias):
+            def __init__(self, syn_count, bias, learning_rate):
+                self.learning_rate = learning_rate
                 self.bias = bias
-                self.weights = np.random.randn(syn_count, 1)
+                self.weights = np.random.randn(syn_count)
+                self.input = 0
+                self.weight_sum = 0
 
             def activate(self, x):
-                s = sigmoid(np.dot(x, self.weights) + self.bias)
-                return s
+                self.input = x
+                self.weight_sum = np.dot(x, self.weights) + self.bias
+                self.output = sigmoid(self.weight_sum)
+                return self.output
+
+            def calculate_err(self, weight_deltas):
+                self.delta = sigmoid(self.weight_sum)*sum(weight_deltas)
+                return self.delta*self.weights
+
+            def reweight(self):
+                pass
 
 
-        def __init__(self, bias, count, syn_per_neuron):
-            self.neurons = [self.Neuron(syn_per_neuron, bias) for i in range(count)]
+        def __init__(self, count, syn_per_neuron, learning_rate):
+            self.inputs = []
+            self.outputs = []
+            self.deltas = None
+            self.bias = np.random.sample()
+            self.neurons = [self.Neuron(syn_per_neuron, self.bias, learning_rate) for i in range(count)]
 
         def feed_forward(self, inputs):
             self.inputs = inputs
             self.outputs = [n.activate(self.inputs) for n in self.neurons]
+            return self.outputs
 
-    def __init__(self, sizes, learning_rate = 0.5):
-        pass
-        #self.layers = [self.NeuralLayer(bias, sizes[i],) for i in range(len(sizes))]
+        def calculate_errs(self, weight_prev_deltas):
+            pre_d = [self.neurons[i].calculate_err(weight_prev_deltas[i]) for i in range(len(self.neurons))]
+            self.deltas = np.array(pre_d).T
 
+        def reweight(self):
+            pass
+
+    def __init__(self, inputs_count, sizes, learning_rate = 0.5):
+        self.layers = []
+        self.sizes = sizes
+        for i in range(len(sizes)):
+            if i == 0:
+                self.layers.append(self.NeuralLayer(sizes[i], inputs_count, learning_rate))
+            else:
+                self.layers.append(self.NeuralLayer(sizes[i], sizes[i-1], learning_rate))
 
     def feed_forward(self, x):
         o = self.layers[0].feed_forward(x)
@@ -37,17 +65,31 @@ class NeuralNetwork:
             o = layer.feed_forward(o)
         return o
 
+    def backprop(self, error):
+        #last layer calculate its weighted deltas
+        # короч, это неправильно - когда обрабатываем последний слой нужно передавать ошибку *  количество нейронов в предпоследнем слое
+        deltas_o = self.layers[-1].calculate_errs(error*self.sizes[-1])
+
+        for layer in self.layers:
+            pass
+
     def train(self, inputs, answers):
         for i in range(len(inputs)):
             output = self.feed_forward(inputs[i])
-            q_errors = np.dot(0.5*(answers[i] - output))
+            error  = answers[i] - output
+            q_error = 0.5*sum(error)
+            self.backprop(error)
 
 
-jimmy = NeuralNetwork([2,2,1])
+jimmy = NeuralNetwork(2, [3,2])
 X = np.array([[0, 1],
               [1, 0],
               [0, 0],
               [1, 1]])
 # output matrix
-Y = np.array([[1, 1, 0, 0]])
-#jimmy.train(X,Y.T)
+Y = np.array([[0, 1],
+              [1, 0],
+              [0, 0],
+              [1, 1]])
+print(np.dot([2,1,3], [-2,1,4]) + 4)
+jimmy.train(X,Y)
