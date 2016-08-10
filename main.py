7843550cef48b1f4d81a3ce60eaee47073b28ea1
normeow <rgdar@yandex.ru>
2016-08-10 18:13:47 +0300

    It works actually

--- main.py
+++ main.py
@@ -14,26 +14,29 @@ class NeuralNetwork:
                 self.learning_rate = learning_rate
                 self.bias = bias
                 self.weights = np.random.randn(syn_count)
-                self.input = 0
+                self.inputs = 0
                 self.weight_sum = 0
 
             def activate(self, x):
-                self.input = x
+                self.inputs = x
                 self.weight_sum = np.dot(x, self.weights) + self.bias
                 self.output = sigmoid(self.weight_sum)
                 return self.output
 
-            def calculate_err(self, weight_deltas):
+            def calculate_err(self, weight_deltas, outp = False):
                 '''
 
                 :param weight_deltas:
                 :return: weighted delta
                 '''
                 self.delta = sigmoid(self.weight_sum, True)*sum(weight_deltas)
+                if outp:
+                    self.delta *= -1
                 return self.delta*self.weights
 
             def reweight(self):
-                pass
+                for i in range(len(self.weights)):
+                    self.weights[i] -= self.learning_rate*self.delta*self.inputs[i]
 
 
         def __init__(self, count, syn_per_neuron, learning_rate):
@@ -48,18 +51,19 @@ class NeuralNetwork:
             self.outputs = [n.activate(self.inputs) for n in self.neurons]
             return self.outputs
 
-        def calculate_errs(self, weight_prev_deltas):
+        def calculate_errs(self, weight_prev_deltas, outp = False):
             '''
 
             :param weight_prev_deltas:
             :return: matrix of weighed deltas
             '''
-            pre_d = [self.neurons[i].calculate_err(weight_prev_deltas[i]) for i in range(len(self.neurons))]
+            pre_d = [self.neurons[i].calculate_err(weight_prev_deltas[i], outp) for i in range(len(self.neurons))]
             self.deltas = np.array(pre_d).T
             return  self.deltas
 
         def reweight(self):
-            pass
+            for neuron in self.neurons:
+                neuron.reweight()
 
     def __init__(self, inputs_count, sizes, learning_rate = 0.5):
         self.layers = []
@@ -78,28 +82,35 @@ class NeuralNetwork:
 
     def backprop(self, error):
         errs = [[error[i]] for i in range(len(error))]
-        deltas_o = self.layers[-1].calculate_errs(errs)
+        deltas_o = self.layers[-1].calculate_errs(errs, True)
 
         for layer in self.layers[-2::-1]:
             deltas_o = layer.calculate_errs(deltas_o)
 
+        for layer in self.layers:
+            layer.reweight()
+
+
     def train(self, inputs, answers):
-        for i in range(len(inputs)):
-            output = self.feed_forward(inputs[i])
-            error  = answers[i] - output
-            q_error = 0.5*sum(error)
-            self.backprop(error)
+
+        for j in range(100000):
+            for i in range(len(inputs)):
+                output = self.feed_forward(inputs[i])
+                error  = answers[i] - output
+                q_error = 0.5*sum(error)
+                if j % 10000 == 0:
+                    print(output)
+                self.backprop(error)
+            if j % 10000 == 0:
+                print()
 
 
-jimmy = NeuralNetwork(2, [3,2])
+
+jimmy = NeuralNetwork(2, [4, 1])
 X = np.array([[0, 1],
               [1, 0],
               [0, 0],
               [1, 1]])
 # output matrix
-Y = np.array([[0, 1],
-              [1, 0],
-              [0, 0],
-              [1, 1]])
-print(np.dot([2,1,3], [-2,1,4]) + 4)
-jimmy.train(X,Y)
+Y = np.array([0,0,0,1])
+jimmy.train(X,Y.T)
